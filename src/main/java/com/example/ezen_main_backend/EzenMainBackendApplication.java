05d6b5a3c886ada41c165ddbb612c7af79d893ad
realdeer <i3295h@naver.com>
2025-01-10 17:37:34 +0900

    first commit

---
+++ src/main/java/com/example/ezen_main_backend/EzenMainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ezen_main_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzenMainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzenMainBackendApplication.class, args);
+	}
+
+}
