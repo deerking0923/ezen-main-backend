7382c09809a881e26ff1dee77405ff4549d8120b
realdeer <i3295h@naver.com>
2025-01-15 15:12:42 +0900

    설정 문제 해결

--- src/main/java/com/springboot/board/EzenMainBackendApplication.java
+++ src/main/java/com/springboot/board/EzenMainBackendApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.project;
+package com.springboot.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
