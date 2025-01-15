227581c853fd165865b1e5075db6e42ca3af9663
realdeer <i3295h@naver.com>
2025-01-15 12:12:45 +0900

    디디렉토리 구조 정리 중

--- src/main/java/com/springboot/project/EzenMainBackendApplication.java
+++ src/main/java/com/springboot/project/EzenMainBackendApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.board;
+package com.springboot.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
